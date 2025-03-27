9cde73a4327be7c67a98e7a570aa25f6c1db5242
yowfung <yowfung@163.com>
2025-03-27 17:58:37 +0800

    test: 添加推理单元测试用例

---
+++ tests/test_inference.py
@@ -0,0 +1,26 @@
+import os
+import time
+import numpy as np
+from spark_tts_lib.SparkTTS import SparkTTS
+
+
+def test_inference():
+    print("⌛️ Loading model...")
+    model_dir = "pretrained_models/Spark-TTS-0.5B"
+    model = SparkTTS(model_dir)
+
+    text = "生活就像海洋，只有意志坚强的人才能到达彼岸。"
+    prompt_speech_path = "tests/prompt_audio.wav"
+    prompt_text = "吃燕窝就选燕之屋，本节目由26年专注高品质燕窝的燕之屋冠名播出。豆奶牛奶换着喝，营养更均衡，本节目由豆本豆豆奶特约播出。"
+
+    print("⌛️ Inferring...")
+    start_at = time.time()
+    wav = model.inference(text, prompt_speech_path, prompt_text)
+
+    elapsed_time = time.time() - start_at
+    print(f"🚀 Inference completed in {elapsed_time:.2f} seconds")
+
+    assert isinstance(wav, np.ndarray)
+    assert wav.ndim == 1
+    assert len(wav) > 1000
+    assert elapsed_time < 20
