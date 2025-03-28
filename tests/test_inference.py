138da86d05face95e085efdf53c1660bba6167f6
yowfung <yowfung@163.com>
2025-03-28 08:27:36 +0800

    test(test_inference.py): 分别用两个测试用例对 CPU 和 CUDA 环境进行测试，并设定测试顺序

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,19 +1,30 @@
 import os
+import pytest
+from pathlib import Path
 import time
 import numpy as np
+import torch
 from spark_tts_lib.SparkTTS import SparkTTS
 from spark_tts_lib.utils.download import download_pretrained_model
 
 
-def test_inference():
+def run_inference_test(device: torch.device):
+    """General inference test function
+
+    Args:
+        device (torch.device): Running device (CPU/GPU)
+    """
     model_dir = "pretrained_models/Spark-TTS-0.5B"
     if not os.path.exists(model_dir):
-        print("🔃 Downloading pretrained model...")
+        print("⌛️ Downloading pretrained model...")
         download_pretrained_model(local_dir=model_dir)
-        print("☑️ Downloaded pretrained model")
+        print("☑️ Downloaded pretrained model.")
 
     print("⌛️ Loading model...")
-    model = SparkTTS(model_dir)
+    start_at = time.time()  
+    model = SparkTTS(model_dir, device)
+    elapsed_time = time.time() - start_at
+    print(f"☑️ Model loaded in {elapsed_time:.2f} seconds.")
 
     text = "生活就像海洋，只有意志坚强的人才能到达彼岸。"
     prompt_speech_path = "tests/prompt_audio.wav"
@@ -24,11 +35,24 @@ def test_inference():
     wav = model.inference(text, prompt_speech_path, prompt_text)
 
     elapsed_time = time.time() - start_at
-    print(f"🚀 Inference completed in {elapsed_time:.2f} seconds")
+    print(f"☑️ Inference completed in {elapsed_time:.2f} seconds.")
 
     assert isinstance(wav, np.ndarray)
     assert wav.ndim == 1
     assert len(wav) > 1000
-    assert elapsed_time < 20
 
-    print("✅ Inference test passed")
+    print("✅ Inference test passed.")
+
+
+@pytest.mark.order(1)
+def test_inference_use_cpu():
+    """Test CPU inference"""
+    run_inference_test(torch.device("cpu"))
+
+
+@pytest.mark.order(2)
+def test_inference_use_cuda():
+    """Test CUDA inference"""
+    if not torch.cuda.is_available():
+        pytest.skip("CUDA is not available")
+    run_inference_test(torch.device("cuda"))
